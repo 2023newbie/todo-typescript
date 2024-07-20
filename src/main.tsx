15683b51a977b818caa76981a0007be5643033e0
Vu Duc <ducwebdev@gmail.com>
2024-07-20 08:00:41 +0700

    chore: setup tanstack-query

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,8 @@ import LoginPage from "./pages/login-page.tsx";
 import SignupPage from "./pages/signup-page.tsx";
 import HomePage from "./pages/home-page.tsx";
 import TodoPage from "./pages/todo-page.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const router = createBrowserRouter([
   {
@@ -34,8 +36,13 @@ const router = createBrowserRouter([
   }
 ])
 
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <UpdateStatusProvider>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </UpdateStatusProvider>,
 );
